a861ae3926ef8e2561761469b61fcf952dfa8eba
Vimukthi Shohan <vimukthi.shohan@gmail.com>
2020-09-05 23:29:40 +0530

    login done

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,85 +1,84 @@
-import { InternalServerErrorException, ValidationError, BadRequestException } from "../util/error"
-import { mobileNumberFormatter } from "../helper/general.helper";
-import User from "../model/user";
-import logger from "../logger";
-import { ApiResponse, ApiError } from "../util/api";
-import { isEmpty } from "lodash";
-import * as jwt from "jsonwebtoken";
+import { InternalServerErrorException, ValidationError, BadRequestException, Exception } from '../util/error'
+import { mobileNumberFormatter } from '../helper/general.helper'
+import User from '../model/user'
+import logger from '../logger'
+import { ApiResponse, ApiError } from '../util/api'
+import { isEmpty } from 'lodash'
+import * as jwt from 'jsonwebtoken'
 require('../config')
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET
 
 export const register = async (req, res, next) => {
-    try {
-        const user = req.body;
-        user.mobile = mobileNumberFormatter(user.mobile);
-        if (await validateUser(user, next)) {
-            const newUser = new User({
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                mobile: user.mobile,
-                password: user.password
-            })
+	try {
+		const user = req.body
+		user.mobile = mobileNumberFormatter(user.mobile)
+		if (await validateUser(user, next)) {
+			const newUser = new User({
+				firstName: user.firstName,
+				lastName: user.lastName,
+				email: user.email,
+				mobile: user.mobile,
+				password: user.password,
+			})
 
-            let createdUser = await newUser.save();
-            createdUser = createdUser.toObject();
-            delete createdUser._id;
-            delete createdUser.password;
-            delete createdUser.__v;
-            delete createdUser.tokens;
+			let createdUser = await newUser.save()
+			createdUser = createdUser.toObject()
+			delete createdUser._id
+			delete createdUser.password
+			delete createdUser.__v
+			delete createdUser.tokens
 
-            logger.info(`New user registration successfull | ${JSON.stringify(createdUser)}`)
-            return res.send(new ApiResponse({user: createdUser}, 'Registration Success!'))
-        }
-
-    } catch (error) {
-        return next(new InternalServerErrorException(error.message, error))
-    }
+			logger.info(`New user registration successfull | ${JSON.stringify(createdUser)}`)
+			return res.send(new ApiResponse({ user: createdUser }, 'Registration Success!'))
+		}
+	} catch (error) {
+		if (error instanceof Exception) return res.send(new ApiError(error.message))
+		else return res.send(new ApiError(error.message, error))
+	}
 }
 
-async function validateUser (user, next) {
-    try {
-        const isExistuserByEmail = await User.findOne({email: user.email})
-        const isExistUserByMobile = await User.findOne({mobile: user.mobile})
-        if (isExistuserByEmail || isExistUserByMobile) {
-            return next(new ValidationError(`New user registration error | Error: User already exists!`))
-        } else {
-            return true;
-        }
-    } catch (error) {
-        return next(new InternalServerErrorException(error.message, error))
-    }
+async function validateUser(user) {
+	try {
+		const isExistuserByEmail = await User.findOne({ email: user.email })
+		const isExistUserByMobile = await User.findOne({ mobile: user.mobile })
+		if (isExistuserByEmail || isExistUserByMobile) {
+			return new ValidationError(`New user registration error | Error: User already exists!`)
+		} else {
+			return true
+		}
+	} catch (error) {
+		return new InternalServerErrorException(error.message, error)
+	}
 }
 
-export const login =async(req, res, next) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
+export const login = async (req, res) => {
+	try {
+		const email = req.body.email
+		const password = req.body.password
 
-        const user = await User.findOne({email})
+		const user = await User.findOne({ email })
 
-        if (isEmpty(user)) return next(new BadRequestException('Email / Password incorrect'));
+		if (isEmpty(user)) throw new BadRequestException('Email / Password incorrect')
 
-        if (!user.comparePassword(password)) return next(new BadRequestException('Email / Password incorrect'))
+		if (!(await user.comparePassword(password))) throw new BadRequestException('Email / Password incorrect')
+		let userObject = {
+			name: `${user.firstName} ${user.lastName}`,
+			email: user.email,
+			mobil: user.mobile,
+		}
 
-        let userObject = {
-            name: `${user.firstName} ${user.lastName}`,
-            email: user.email,
-            mobil: user.mobile
-        }
+		const token = jwt.sign(userObject, secret, {
+			expiresIn: process.env.LOGGED_IN_TOKEN_EXPIRATION,
+		})
+		user.tokens.push(token)
 
+		await user.save()
 
-        const token = jwt.sign(userObject, secret, {
-            expiresIn: process.env.LOGGED_IN_TOKEN_EXPIRATION,
-        });
-        user.tokens.push(token);
-
-        await user.save();
-
-        logger.info(`User logged in | user: ${userObject.email}`);
-        return res.send(new ApiResponse({user: userObject, token}, 'Login Success!'))
-
-    } catch (error) {
-        next(new InternalServerErrorException(error.message, error))
-    }
+		logger.info(`User logged in | user: ${userObject.email}`)
+		return res.send(new ApiResponse({ user: userObject, token }, 'Login Success!'))
+	} catch (error) {
+		logger.error(error.message)
+		if (error instanceof Exception) return res.send(new ApiError(error.message))
+		else return res.send(new ApiError('InternalServerErrorException | ' + error.message))
+	}
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { ApiResponse } from './util/api'
 import logger from './logger'
-import { globalErrorHandler } from './util/error'
 import cors from 'cors'
 import httpLogger from './middleware/httplogger.middleware'
-import auth from "./router/auth.route";
-import home from "./router/home.route";
+import auth from './router/auth.route'
+import home from './router/home.route'
 import { jwt } from './middleware/jwtverify.middleware'
 
 const api = express()
@@ -16,10 +15,10 @@ const api = express()
 // ==================================================== middleware =====================================================
 api.use(cors())
 api.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
+	res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+	next()
+})
 api.use(bodyParser.json())
 api.use(bodyParser.urlencoded({ extended: true }))
 api.use(httpLogger)
@@ -28,13 +27,10 @@ api.use(httpLogger)
 // root endpoint
 api.all('/', (req, res) => res.send(new ApiResponse({}, 'API has started and working!')))
 
-const base_url = endpoint => `/${endpoint}`;
+const base_url = endpoint => `/${endpoint}`
 
 api.use(base_url('auth'), auth)
 api.use(base_url('home'), jwt, home)
-
-// global error handler
-api.use(globalErrorHandler)
 
 // ====================================================== LISTEN =======================================================
 api.listen(process.env.PORT, error => {
